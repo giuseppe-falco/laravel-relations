f8439dc50ed3a36e5501def2f8fc2ce08695e74a
peppe965 <giuseppe.falco000@gmail.com>
2020-11-03 21:13:59 +0100

    seeder Comic ok

--- app/Comic.php
+++ app/Comic.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-
+    protected $fillable = [
+        "title",
+        "original_title",
+        "author",
+        "number",
+        "n_pages",
+        "edition",
+        "reading",
+        "price",
+        "color",
+        "release",
+        "cover",
+    ];
 }
--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comic;
+use Faker\Generator as Faker;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -9,8 +11,31 @@ class ComicsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        //$faker = Faker\Factory::create();
 
+
+        for ($i=0; $i < 10; $i++) { 
+
+            $newComic = new Comic;
+
+            $newComic->title = $faker->word;
+            
+            $newComic->original_title = $faker->word;
+
+            $newComic->author = $faker->name;
+            $newComic->number = $faker->numberBetween(1,1000);
+            $newComic->n_pages = $faker->numberBetween(20,400);
+            $newComic->edition = $faker->company();
+            $newComic->reading = (rand(0,1) == 1) ? "ltr" : "rtl";
+            $newComic->price = $faker->numberBetween(1,100);
+            $newComic->color = ( rand(0,1) == 1) ? true : false ;
+            $newComic->release = $faker->year($max = 'now');
+            $newComic->cover = $faker->imageUrl($width = 300, $height = 300);
+
+            $newComic->save();
+
+        }
     }
 }
